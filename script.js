9c4c510d7ba85d54d9227df620d924efb94d30c9
Lucas Barros <119129385+BarrosPL@users.noreply.github.com>
2024-09-30 10:45:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("btn").onclick = function() {
+    window.location.href = "https://go.pepper.com.br/xj0cm"; }
+
+    document.addEventListener('DOMContentLoaded', function() {
+        var video = document.getElementById('myVideo');
+        video.play();
+    });
